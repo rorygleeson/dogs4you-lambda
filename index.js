9cd1e4d417aa047eeca7bca6a3a5944890f4263e
rorygleeson <rory@skygrid.io>
2025-04-09 17:34:33 +1000

    Add google analytics

--- index.js
+++ index.js
@@ -124,6 +124,17 @@ function generateHTML(videos) {
             display: inline-block;
         }
     </style>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-69T8E57ETV"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-69T8E57ETV');
+    </script>
+    
 </head>
 <body>
     <header>
